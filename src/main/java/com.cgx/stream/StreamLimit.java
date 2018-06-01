3ea40bedce405be4370e26be3f5eb66af6370769
rqwxin <605760016@qq.com>
2018-06-01 10:37:59 +0800

    ww

--- src/main/java/com.cgx/stream/StreamLimit.java
+++ src/main/java/com.cgx/stream/StreamLimit.java
@@ -10,6 +10,7 @@ public class StreamLimit {
 		Stream se = Stream.of("a","b","c","d","e");
 		//limit 返回 Stream 的前面 n 个元素
 		List<String> s =  (List<String>) se.limit(3).collect(Collectors.toList());
+		System.out.println(s.size());
 		s.forEach(System.out::print);
 	}
 }
